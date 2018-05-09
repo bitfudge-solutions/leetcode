9c84331b5c7210cfe7eecdb41e586a1342287112
bitfudge-solutions <mail@bitfudge.solutions>
2018-05-09 07:49:23 +0530

    Solution for judge-route-circle

--- judge-route-circle/Solution.cpp
+++ judge-route-circle/Solution.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
   bool judgeCircle(const string &moves) {
-    int x_movements(0);
-    int y_movements(0);
+    int count[26] = {0};
 
-    for (const char direction: moves) {
-      if ('U' == direction)
-        ++y_movements;
-      else if ('D' == direction)
-        --y_movements;
-      else if ('L' == direction)
-        --x_movements;
-      else if ('R' == direction)
-        ++x_movements;
-    }
+    for (const char move: moves)
+      ++count[move - 'A'];
 
-    return 0 == x_movements && 0 == y_movements;
+    return 0 == (count['U' - 'A'] - count['D' - 'A']) &&
+      0 == (count['L' - 'A'] - count['R' - 'A']);
   }
 };
